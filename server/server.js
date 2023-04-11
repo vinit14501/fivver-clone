9928ad7c7267fedc3e44751eec54505e27a5b642
vinit14501 <57012278+vinit14501@users.noreply.github.com>
2023-04-11 12:33:35 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -4,38 +4,38 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
-// import userRoute from './routes/user.route.js';
-// import gigRoute from './routes/gig.route.js';
-// import orderRoute from './routes/order.route.js';
-// import conversationRoute from './routes/conversation.route.js';
-// import messageRoute from './routes/message.route.js';
-// import reviewRoute from './routes/review.route.js';
-// import authRoute from './routes/auth.route.js';
+import userRoute from './routes/user.route.js';
+import gigRoute from './routes/gig.route.js';
+import orderRoute from './routes/order.route.js';
+import conversationRoute from './routes/conversation.route.js';
+import messageRoute from './routes/message.route.js';
+import reviewRoute from './routes/review.route.js';
+import authRoute from './routes/auth.route.js';
 
 const app = express();
 dotenv.config();
 mongoose.set('strictQuery', true);
 
-// const connect = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO);
-//     console.log('Connected to mongoDB!');
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('Connected to mongoDB!');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use('/api/auth', authRoute);
-// app.use('/api/users', userRoute);
-// app.use('/api/gigs', gigRoute);
-// app.use('/api/orders', orderRoute);
-// app.use('/api/conversations', conversationRoute);
-// app.use('/api/messages', messageRoute);
-// app.use('/api/reviews', reviewRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/gigs', gigRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/conversations', conversationRoute);
+app.use('/api/messages', messageRoute);
+app.use('/api/reviews', reviewRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
@@ -44,7 +44,8 @@ app.use((err, req, res, next) => {
   return res.status(errorStatus).send(errorMessage);
 });
 
-app.listen(8800, () => {
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
   connect();
-  console.log('Backend server is running!');
+  console.log(`Server running on http://localhost:${PORT}`);
 });
